a171269ca9d889bb964bb9d9881651778e046d41
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-28 15:01:26 +0530

    Update tts.sh

--- tts.sh
+++ tts.sh
@@ -91,8 +91,8 @@ install_deps(){
         apt update && apt upgrade -y
         pkg install python
         cd $HOME
-        mkdir storage/music
-        mkdir storage/music/ttsVoice
+        mkdir storage/emulated/0/music
+        mkdir storage/emulated/0/music/ttsVoice
         cd $HOME
         cd text-to-speech
         
