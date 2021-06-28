6fb8449605fa9f3357f62c04d4546ab8b38c9824
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-28 15:12:32 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,9 +7,11 @@ clear
 
 apt update && apt upgrade
 
+cd $HOME
+
 rm -rf text-to-speech
 
-git clone https://github.com/m
+git clone https://github.com/m47r1x-2/text-to-speech.git
 
 cd text-to-speech
 
